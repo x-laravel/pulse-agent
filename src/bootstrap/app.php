4115a543d4ce3cb72adadcc94ee3091447e5d66e
X-Adam <e-posta@mehmetogmen.com.tr>
2026-03-27 01:35:24 +0300

    Refactor application configuration to improve readability and structure

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -5,11 +5,12 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Laravel\Pulse\Contracts\Storage;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })
-    ->create()
-    ->tap(function (Application $app) {
-        $app->bind(Storage::class, PulseDatabaseStorage::class);
-    });
+    ->create();
+
+$app->bind(Storage::class, PulseDatabaseStorage::class);
+
+return $app;
